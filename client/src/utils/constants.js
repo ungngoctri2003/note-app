1adfdd102804e7713a60473e3c0cf9ddb71ae1fe
Leo Nguyen <leo.nguyen@dssolution.jp>
2025-04-27 14:33:38 +0700

    feat: update GraphQL server and subscription endpoint to production URL

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const GRAPHQL_SERVER = "http://localhost:4000";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000/graphql";
+export const GRAPHQL_SERVER = "https://note-app-t0bz.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
+  "ws://https://note-app-t0bz.onrender.com/graphql";
